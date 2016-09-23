0135ccd8cb02e77a8d938811360b7aa623305a18
Marek Safar <marek.safar@gmail.com>
2016-09-23 13:36:11 +0200

    [System.Core] Fixes warnings

--- mcs/class/corlib/System.Security.AccessControl/NativeObjectSecurity.cs
+++ mcs/class/corlib/System.Security.AccessControl/NativeObjectSecurity.cs
@@ -129,12 +129,7 @@ namespace System.Security.AccessControl
 		{
 			Persist (name, includeSections, null);
 		}
-
-		internal void Persist (SafeHandle handle)
-		{
-			PersistModifications (handle);
-		}
-		
+	
 		internal void PersistModifications (SafeHandle handle)
 		{
 			WriteLock();
